124301cf5631f09a3ee5261caba6afcaf888aa1f
Orion <orionvtg@gmail.com>
2020-10-16 19:18:38 -0700

    finishing touches and linking the generate button to the js file

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
 // Assignment Code
+var chars = ["a", "b", "c", "d"]
+var nums = [1, 2, 3, 4, 5, 6, 7, 8,]
+var sChars = ["@", "$", "!", "(", "^"]
+var emojis = ["😁", "😎", "🤞", "✌"]
+var possibles = []
+var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
+
+function generate(){
+  var possibles = []
+
+  // prompt length of pass
+var length = parseInt(prompt("How long does this password need to be?"))
+
+// character type prompts
+if (length <8 || length > 128 || isNaN(length)) {
+  alert("no can do, refresh the page")
+} else {
+  var charsTrue = confirm("do you want chars?")
+  var numsTrue = confirm("do you want numbers?")
+  var sCharsTrue = confirm("do you want special characters")
+  var emojisTrue = confirm("do you want emojis")
+
+  if(charsTrue) {possibles.push(chars)}
+  if(numsTrue) {possibles.push(nums)} 
+  if(sCharsTrue) {possibles.push(sChars)}
+  if(emojisTrue) {possibles.push(emojis)}
+
+
+  var pw = ""
+
+  while (pw.length < length) {
+    for(let i = 0; i < possibles.length; i++) {
+      if (pw.length < length) {
+        let rand = Math.floor(Math.random() * possibles[i].length)
+        pw += possibles[i][rand]
+      }
+    }
+  }
+  console.log(pw)
+  passwordText.value = pw;
+
+}
+}
+
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generate);
